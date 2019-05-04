0e69d8e8ad169180cc468074c032baf1202d9f1d
hewagekasunthilina <kasunthilina1000@gmail.com>
2019-05-04 13:01:04 +0530

    imges added

---
+++ MUSICA/src/com/onlinemusicstore/service/IUserService.java
@@ -0,0 +1,19 @@
+package com.onlinemusicstore.service;
+
+import java.util.ArrayList;
+
+import com.onlinemusicstore.model.User;
+
+public interface IUserService {
+	
+	public void addUser(User user);
+	public User updateUser(User user);
+	public User loginUser(User user);
+	public String getpassword(String userID);
+	public void updatepassword(String userID, String password);
+	public ArrayList<User> getUser(String userID);
+	public ArrayList<String> getUserIDs();
+	public void deleteUser(String userID);
+	
+
+}
